3c505c93113467c4ec049f752062d3b1113cfec9
sujannani <sujan.kommalapati@gmail.com>
2023-11-11 13:45:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Home from './pages/home';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Login/>
     </div>
   );
 }
