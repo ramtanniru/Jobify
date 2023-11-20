44edfce98340c874d2443041c26f602ecc2f83d0
unknown <https://github.com/Likhithacodes/react1.git>
2023-11-20 10:49:38 +0530

    team

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import img from '../assets/img1.svg'
+import ram from '../assets/ram.jpg';
+import preethi from '../assets/preethi.jpg';
+import likhitha from '../assets/likhitha.jpg';
+import img from '../assets/img1.svg';
+import sujan from '../assets/sujan.jpg';
+import praneeth from '../assets/praneeth.jpg'
 import '../Style.css';
 export default function Team(){
     return(
@@ -8,37 +13,37 @@ export default function Team(){
             <div className='d-flex flex-row justify-content-between w-100'>
             <div className='card-container mx-2'>
                 <div className='image-container'>
-                    <img src={img} alt="Teammate 1" className='img' />
-                    <div className='p-2'><h4>Name</h4>
-                    <p>About the person...</p></div>
+                    <img src={ram} alt="Teammate 1" className='img' />
+                    <div className='p-2'><h4>T.Leela Sai Ram</h4>
+                    <p>Frontend and Backend Developer</p></div>
                     </div>
             </div>
             <div className='card-container mx-1'>
                 <div className='image-container'>
-                    <img src={img} alt="Teammate 2" className='img' />
-                    <div className='p-2'><h4>Name</h4>
-                    <p>About the person...</p></div>
+                    <img src={sujan} alt="Teammate 2" className='img' />
+                    <div className='p-2'><h4>K.Sujan</h4>
+                    <p>Backend Developer</p></div>
                     </div>
             </div>
             <div className='card-container mx-1'>
                 <div className='image-container'>
-                    <img src={img} alt="Teammate 1" className='img' />
-                    <div className='p-2'><h4>Name</h4>
-                    <p>About the person...</p></div>
+                    <img src={likhitha} alt="Teammate 1" className='img' />
+                    <div className='p-2'><h4>M.Likhitha</h4>
+                    <p>Frontend Developer</p></div>
                     </div>
             </div>
             <div className='card-container'>
                 <div className='image-container'>
-                    <img src={img} alt="Teammate 1" className='img' />
-                    <div className='p-2'><h4>Name</h4>
-                    <p>About the person...</p></div>
+                    <img src={preethi} alt="Teammate 1" className='img' />
+                    <div className='p-2'><h4>M.Preethi</h4>
+                    <p>UI and Frontend Developer</p></div>
                     </div>
             </div>
             <div className='card-container'>
                 <div className='image-container'>
-                    <img src={img} alt="Teammate 1" className='img' />
-                    <div className='p-2'><h4>Name</h4>
-                    <p>About the person...</p></div>
+                    <img src={praneeth} alt="Teammate 1" className='img' />
+                    <div className='p-2'><h4>S.Praneeth</h4>
+                    <p>UI Designer</p></div>
                     </div>
             </div>
         </div>
